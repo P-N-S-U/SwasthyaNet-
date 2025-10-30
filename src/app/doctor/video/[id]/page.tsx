9f1ff14a5463ffd91e53d3ca38ad9ab649860293
Priyanshu Malusare <priyanshuct3@gmail.com>
2025-10-30 12:32:16 +0000

    ReferenceError: AlertDialogTrigger is not defined     at DoctorVideoCall

--- src/app/doctor/video/[id]/page.tsx
+++ src/app/doctor/video/[id]/page.tsx
@@ -31,6 +31,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { completeAppointment } from '@/app/actions/appointments';
 import { useToast } from '@/hooks/use-toast';
